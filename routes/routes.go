40a305ebd08ecdc43a11b778f24746a7336a7940
chandan <chandanpullanikatt@gmail.com>
2025-11-18 01:43:38 +0530

    Added CORS

--- routes/routes.go
+++ routes/routes.go
@@ -10,31 +10,28 @@ import (
 func SetupRouter() *gin.Engine {
     r := gin.Default()
 
-    // CORS -- SET THIS LIST TO INCLUDE ALL YOUR FRONTEND HOSTS (local and deployed)
-    r := gin.Default()
-r.Use(cors.New(cors.Config{
-    AllowOrigins: []string{
-        "http://localhost:5173",
-        "http://trading-dashboard-chandan.s3-website.eu-north-1.amazonaws.com",
-        "https://doui0bo89ibz6.cloudfront.net",
-    },
-    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowHeaders:     []string{"Content-Type", "Authorization"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: false,
-}))
-
+    // CORS
+    r.Use(cors.New(cors.Config{
+        AllowOrigins: []string{
+            "http://localhost:5173",
+            "http://trading-dashboard-chandan.s3-website.eu-north-1.amazonaws.com",
+            "https://doui0bo89ibz6.cloudfront.net",
+        },
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: false,
+    }))
 
     // Public endpoints
     r.GET("/prices", controllers.GetPrices)
     r.GET("/ws", controllers.WSHandler)
 
-    // Auth (login)
+    // Auth
     authGroup := r.Group("/auth")
     authGroup.POST("/login", auth.LoginHandler)
-    // If you later allow registration, add /register here.
 
-    // Orders protected by JWT
+    // Orders (protected)
     orders := r.Group("/orders")
     orders.Use(auth.AuthMiddleware())
     orders.GET("", controllers.GetOrders)
